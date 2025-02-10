e837965164ab1b126921fbd1e162ca68be58e631
Danyal Rashid <danyalr8@gmail.com>
2025-02-11 00:06:59 +0500

    Controller Logic

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class AuthController extends Controller {
+    public function login(Request $request) {
+        $credentials = $request->validate([ 'email' => 'required|email', 'password' => 'required' ]);
+        if (!Auth::attempt($credentials)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+        return ['token' => $request->user()->createToken('authToken')->plainTextToken];
+    }
+}
