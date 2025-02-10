68874fd6615837b302b44b53208782605acb4ed8
Danyal Rashid <danyalr8@gmail.com>
2025-02-11 00:10:22 +0500

    Logout

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,5 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\AuthController;
@@ -8,3 +9,4 @@ Route::middleware('auth:sanctum')->group(function () {
 });
 
 Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout']);
