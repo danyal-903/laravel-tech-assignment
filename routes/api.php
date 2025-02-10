891fc9363ccd1770561586baa13ec8144245d977
Danyal Rashid <danyalr8@gmail.com>
2025-02-11 00:03:29 +0500

    Settings

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\AuthController;
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('products', ProductController::class);
+});
+
+Route::post('login', [AuthController::class, 'login']);
